7ad8770529143d4c36032494f2dda52f554dd995
Florian Wiesweg <f.wiesweg@tu-berlin.de>
2019-07-02 16:57:26 +0200

    Add route setup

--- userweb/src/app/app-routing.module.ts
+++ userweb/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { RankingComponent } from './ranking/ranking.component';
+
+const routes: Routes = [
+  { path: 'ranking', component: RankingComponent },
+  { path: '', redirectTo: '/ranking', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
