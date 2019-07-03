17ddd40e46610f46a99cff6bc91d7b14cc0bfe86
Florian Wiesweg <f.wiesweg@tu-berlin.de>
2019-07-03 13:24:52 +0200

    Add context controls

--- userweb/src/app/app.module.ts
+++ userweb/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RankingComponent } from './ranking/ranking.component';
 
-import { MatButtonModule, MatCheckboxModule, MatChipsModule, MatSliderModule, MatSlideToggleModule, MatToolbarModule } from '@angular/material';
+import {
+  MatButtonModule, MatCheckboxModule, MatChipsModule,
+  MatFormFieldModule, MatSelectModule, MatSliderModule,
+  MatSlideToggleModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -18,9 +22,13 @@ import { MatButtonModule, MatCheckboxModule, MatChipsModule, MatSliderModule, Ma
     AppRoutingModule,
     BrowserAnimationsModule,
 
+    HttpClientModule,
+
     MatButtonModule,
     MatCheckboxModule,
     MatChipsModule,
+    MatFormFieldModule,
+    MatSelectModule,
     MatSliderModule,
     MatSlideToggleModule,
     MatToolbarModule
